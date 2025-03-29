15201d370f2a8a57a85a74374e2da9fb9e997100
amarnath-dev-dsa <ghoshamarnath06@gmail.com>
2025-03-29 19:47:38 +0530

    third commit

---
+++ script.js
@@ -0,0 +1,39 @@
+function hamburg() {
+    const navbar = document.querySelector(".dropdown");
+    navbar.style.transform = "translateY(0px)";
+}
+
+function cancel() {
+    const navbar = document.querySelector(".dropdown");
+    navbar.style.transform = "translateY(-500px)";
+}
+
+
+const texts = ["DEVELOPER", "CODER", "STUDENT"];
+let speed = 100;
+const textElement = document.querySelector(".typewriter-text");
+let textIndex = 0;
+let charIndex = 0;
+
+function typeWriter() {
+    if (charIndex < texts[textIndex].length) {
+        textElement.innerHTML += texts[textIndex].charAt(charIndex);
+        charIndex++;
+        setTimeout(typeWriter, speed);
+    } else {
+        setTimeout(eraseText, 1000);
+    }
+}
+
+function eraseText() {
+    if (textElement.innerHTML.length > 0) {
+        textElement.innerHTML = textElement.innerHTML.slice(0, -1);
+        setTimeout(eraseText, 50);
+    } else {
+        textIndex = (textIndex + 1) % texts.length;
+        charIndex = 0;
+        setTimeout(typeWriter, 500);
+    }
+}
+
+window.onload = typeWriter;
